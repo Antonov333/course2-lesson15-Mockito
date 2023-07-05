24b146129a3d220ebd2f09a461c79cbfb4c56b57
Antonov333 <antonovsergei333@yahoo.com>
2023-07-05 15:06:06 +0300

    Initial commit

---
+++ src/main/java/com/example/course2lesson15mockitohomework/Course2Lesson15MockitoHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.course2lesson15mockitohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson15MockitoHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson15MockitoHomeworkApplication.class, args);
+    }
+
+}
